55cfeb0875c8ab5b7fbc7df89593f659800f4b2b
muhfaishalrizal <muhammadfaishalrizal27@gmail.com>
2021-06-12 20:50:15 +0700

    Tugas 08

--- src/main/java/treeASD2/No2.java
+++ src/main/java/treeASD2/No2.java
@@ -1,11 +1,18 @@
 package treeASD2;
 public class No2 {
-//        a) D merupakan Predesor dari B dan F
-//        b) B dan F merupakan Succesor dari D
-//        c) B dan D merupakan Ancestor dari C atau A
-//        d) C dan B merupakan Descendent dari D
-//        e) B merupakan Parent dari C
-//        f) C merupakan Child dari B
-//        g) C dan A adalah Sibling
-//        h) Subtree B,A,C dan Subtree F,E,G
+////        a) D merupakan Predesor dari B dan F
+////           B merupakan Prodesor dari A  dan C
+////        b) B dan F merupakan Succesor dari D
+////           A dan C merupakan Succseor dari B
+////        c) B dan D merupakan Ancestor dari C atau A
+////           F dan D merupakan Ancestor dari E atau G
+////        d) C dan B merupakan Descendent dari D
+////           E dan F merupakan Descendent dari D
+////        e) B merupakan Parent dari C
+////           F merupakan parent dari G 
+////        f) C merupakan Child dari B
+////           E merupakan Child dari F
+////        g) C dan A adalah Sibling
+////           E dan F adalah Sibling
+////        h) Subtree B,A,C dan Subtree F,E,G
 }
